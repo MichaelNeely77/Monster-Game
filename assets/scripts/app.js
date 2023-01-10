39f2fc3eca4b822e060f293fff671e8cdd8d99f5
Michael Neely <michael@michaelneely.net>
2023-01-10 09:48:29 -0500

    Streamlined code by consolidating fuctions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,7 +8,13 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode = 'STRONG_ATTACK') {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -18,8 +25,21 @@ function attackHandler() {
     }  else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You lost!');
     } else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-        alert('"YOu have a draw!"');
+        alert('"You have a draw!"');
     }
 }
 
+
+function attackHandler() {
+
+    attackMonster('ATTACK');
+
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
+   
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
