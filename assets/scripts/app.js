2b83de0c53e78798b6a7d497815e22e7d7219f16
Michael Neely <michael@michaelneely.net>
2023-01-14 15:49:32 -0500

    Added reset feature

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,7 +13,7 @@ adjustHealthBars(chosenMaxLife);
 function reset() {
     currentMonsterHealth = chosenMaxLife;
     currentPlayerHealth = chosenMaxLife;
-    resetGame();
+    resetGame(chosenMaxLife);
 }
 
 
@@ -33,15 +33,20 @@ function endRound() {
 
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert("You won!");
-        reset();
     }  else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You lost!');
-        reset();
     } else if(currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
         alert('"You have a draw!"');
+    }
+
+    if (currentMonsterHealth <= 0 || currentPlayerHealth <=0) {
         reset();
     }
 }
+
+
+
+
 
 
 function attackMonster(mode) {
