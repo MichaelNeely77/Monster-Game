08e548d4d931d9010c2d537f5e39f1a74677f012
Michael Neely <michael@michaelneely.net>
2023-01-18 10:12:44 -0500

    Added a 'switch' statement instead of four 'if' statements ion the writeToLog function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -33,26 +33,56 @@ function writeToLog (ev, val, monsterHealth, playerHealth) {
         finalMonsterHealth: monsterHealth,
         finalPlayerHealth: playerHealth
 };
-    if (ev === LOG_EVENT_PLAYER_ATTACK) {
-        logEntry.target = 'MONSTER';
-    } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
-        logEntry = {
-            event: ev,
-            value: val,
-            target: 'MONSTER',
-            finalMonsterHealth: monsterHealth,
-            finalPlayerHealth: playerHealth
-        };
-        
-    }else if (ev === LOG_EVENT_MONSTER_ATTACK) {
-        logEntry.target = 'PLAYER';
-    
-    } else if (ev === LOG_EVENT_PLAYER_HEAL) {
-        logEntrytarget = 'PLAYER';
-    
-    } else if (LOG_EVENT_GAME_OVER) {
-        
+    switch (ev) {
+        case LOG_EVENT_PLAYER_ATTACK:
+            logEntry.target = 'MONSTER';
+            break;
+        case LOG_EVENT_PLAYER_STRONG_ATTACK:
+            logEntry = {
+                event: ev,
+                value: val,
+                target: 'MONSTER',
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+            break;
+        case LOG_EVENT_MONSTER_ATTACK:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_PLAYER_HEAL:
+            logEntry.target = 'PLAYER';
+            break;
+        case LOG_EVENT_GAME_OVER:
+            logEntry = {
+                event: ev,
+                value: val,
+                finalMonsterHealth: monsterHealth,
+                finalPlayerHealth: playerHealth
+            };
+            break;
+            default: 
+                logEntry = {};
     }
+    // if (ev === LOG_EVENT_PLAYER_ATTACK) {
+    //     logEntry.target = 'MONSTER';
+    // } else if (ev === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+    //     logEntry = {
+    //         event: ev,
+    //         value: val,
+    //         target: 'MONSTER',
+    //         finalMonsterHealth: monsterHealth,
+    //         finalPlayerHealth: playerHealth
+    //     };
+        
+    // }else if (ev === LOG_EVENT_MONSTER_ATTACK) {
+    //     logEntry.target = 'PLAYER';
+    
+    // } else if (ev === LOG_EVENT_PLAYER_HEAL) {
+    //     logEntrytarget = 'PLAYER';
+    
+    // } else if (LOG_EVENT_GAME_OVER) {
+        
+    // }
     battleLog.push(logEntry);
 }
 
